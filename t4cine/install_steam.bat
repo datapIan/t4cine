ea15cd8508f64c95596647edf50173e7b7bc65c0
datapIan <81714507+datapIan@users.noreply.github.com>
2023-08-04 20:39:51 -0700

    Delete install_steam.bat

--- t4cine/install_steam.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-md "%localappdata%\Activision\CoDWaW\mods"
-xcopy /s "%cd%\mp_t4cine" "%localappdata%\Activision\CoDWaW\mods\mp_t4cine\"
-echo t4cine (steam) installed successfully.
-pause
