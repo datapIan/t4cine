084dd24b4b334e66601917fba4fb9422226a2ccf
datapIan <81714507+datapIan@users.noreply.github.com>
2023-08-04 20:39:44 -0700

    Delete install_pluto.bat

--- t4cine/install_pluto.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-md "%localappdata%\Plutonium\storage\t4\mods"
-xcopy /s "%cd%\mp_t4cine" "%localappdata%\Plutonium\storage\t4\mods\mp_t4cine\"
-echo t4cine (pluto) installed successfully.
-pause
